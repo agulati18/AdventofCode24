360abaa2576a531e49504771404f34dca14c4a5e
agulati18 <67696969+agulati18@users.noreply.github.com>
2024-12-04 23:02:11 +0000

    Day 3 Puzzle 2

--- Day3/main.py
+++ Day3/main.py
@@ -12,9 +12,9 @@ matches = re.findall(pattern, input)
 
 # Extract only the numbers from the matches (re.findall(r'\d+,\d+', ",")) and convert them to integers
 numbers = [(int(x), int(y)) for x, y in (match.split(',') for match in re.findall(r'\d+,\d+', ",".join(matches)))]
-print(numbers)
+#print(numbers)
 
-# Aggregate Sum Counter 
+# Aggregate Sum Counter
 aggregate_sum = 0
 
 # Iterate over the numbers, multiply the pairs, and calculate the sum
@@ -23,9 +23,56 @@ for x, y in numbers:
     aggregate_sum += product
 
 # Print the Result
-print(aggregate_sum)
-  
+print(f'Aggregate Sum: {aggregate_sum}')
 
-  
-# aggregated_result += (x * y)
-# print(aggregated_result)
+### Puzzle 2
+'''
+1. The do() instruction enables future mul instructions. do() is on. Everything after it, till a dont() is reached is valid for being included in final sum.
+
+2. The don't() instruction disables future mul instructions. don't() is off. Everything after it, till a do() is reached is invalid for being included in final sum.
+
+Only the most recent do() or don't() instruction applies.
+At the beginning of the program, mul instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. 
+
+The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+'''
+
+# Cut down this overall input into a subspace, with only do() and dont() and mul(x,y) instructions
+
+# Define the regex pattern for matches to keep
+pattern = r"do\(\)|don't\(\)|mul\(\d+,\d+\)"
+
+# Find all matches in the input string
+matches = re.findall(pattern, input)
+
+# In this for loop, I want to iterate over the matches. If the mul(x,y) is after a do() or not after a dont() instruction, then I want to add it to the aggregate sum. If the mul(x,y) is after a don't() instruction, then I don't want to add it to the aggregate sum.
+aggregated_sum = 0
+# Track state if mul instructions are enabled
+enabled = True
+
+for match in matches:
+  if match == "do()":
+      enabled = True
+  elif match == "don't()":
+      enabled = False
+  # Using a while loop here leads to an infinite loop on the first element because the 1st element is enabled by default. Use if.
+  if enabled:
+    numbers = re.findall(r'\d+', match)
+    #print(numbers)
+    if len(numbers) != 0:
+      x, y = map(int, numbers)
+      #print(x, y)
+      product = x * y
+      aggregated_sum += product
+
+# Print the aggregate sum
+print(f'Aggregated Sum (with enabler): {aggregated_sum}')
